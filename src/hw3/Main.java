08dae7067b608e0dd51660b3af0d851ec74e018b
p_Ilya <petermoloch@mail.ru>
2021-11-20 10:43:46 +0300

    homework #3: main tasks completed

--- src/hw3/Main.java
+++ src/hw3/Main.java
@@ -7,6 +7,13 @@ public class Main {
         printLine(2);
         task2();
         printLine(3);
+        task3();
+        printLine(4);
+        task4();
+        printLine(5);
+        task5();
+        printLine(6);
+
     }
 
     public static void printLine(int num){
@@ -40,4 +47,55 @@ public class Main {
         }
         System.out.println();
     }
+
+    public static void printMass(int[] mass){
+        for (int i=0; i<mass.length; i++) {
+            System.out.print(mass[i]+"\t");
+        }
+        System.out.println();
+    }
+
+    public static void task3(){
+        int[] mass = { 1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1 };
+        System.out.println("Начальный массив:");
+        printMass(mass);
+        System.out.println("Отредактированный массив:");
+        for (int i=0;i<mass.length;i++){
+            if (mass[i]<6) mass[i]*=2;
+            System.out.print(mass[i]+"\t");
+        }
+        System.out.println();
+    }
+
+    public static void task4(){
+        int[][] mass = new int[5][5];
+        for (int i=0; i<5; i++){
+            for (int j=0; j<5; j++){
+                mass[i][j] = (i==j ? 1 : 0);
+            }
+        }
+        System.out.println("Получившийся двухмерный массив:");
+        for (int i=0;i<5; i++){
+            printMass(mass[i]);
+        }
+    }
+
+    public static int[] initMass(int length, int initValue, boolean fix){
+        int[] mass = new int[length];
+        for (int i=0; i<length; i++) {
+            if (fix) {
+                mass[i] = initValue;
+            } else {
+                mass[i] = (int) (Math.random()*initValue);
+            }
+        }
+        return mass;
+    }
+
+    public static void task5(){
+        int[] mass = initMass(7,33, true);
+        System.out.println("Инициированный массив:");
+        printMass(mass);
+    }
+
 }
