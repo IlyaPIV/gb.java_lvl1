dc789869931aa8374a68848823aa3ae543306772
IlyaPIV <petermoloch@gmail.com>
2021-12-07 15:29:52 +0300

    homework #7: complete

---
+++ src/hw7/Plate.java
@@ -0,0 +1,31 @@
+package hw7;
+
+public class Plate {
+    private int food;
+
+    public Plate(int food){
+        this.food = food;
+    }
+
+    public void info(){
+        System.out.println("остаток еды в тарелке = "+food);
+    }
+
+    public boolean decreaseFood(int n, Cat cat){
+        if (food == 0) {
+            System.out.println("Тарелка пуста.");
+            increaseFood(50);
+            return false;
+        } else {
+            int count = (n<=food ? n : 0);
+            food -= count;
+            System.out.printf("Кот %s съел %d единиц корма\n",cat.getName(),count);
+            return n<=food;
+        }
+    }
+
+    public void increaseFood(int food){
+        this.food += food;
+        System.out.printf("В тарелку досыпали %d единиц корма.\n",food);
+    }
+}
